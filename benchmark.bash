c86ef398db3048f896580e1841b46e5210402ff3
budopod1 <51675874+budopod1@users.noreply.github.com>
2024-01-03 03:12:20 +0000

    Changed: Epsilon code uses addition/assignment operator | Reordered: benchmarking order

--- benchmark.bash
+++ benchmark.bash
@@ -2,22 +2,22 @@
 set -e
 cd "${0%/*}"
 
-echo "Benchmarking C..."
-time C/main
-echo
-
 echo "Benchmarking C#..."
 time mono Csharp/Main.exe
+echo
+
+echo "Benchmarking Rust..."
+time Rust/main
 echo
 
 echo "Benchmarking Epsilon..."
 time Epsilon/main
 echo
 
-echo "Benchmarking Python..."
-time python -OO Python/main.py
+echo "Benchmarking C..."
+time C/main
 echo
 
-echo "Benchmarking Rust..."
-time Rust/main
+echo "Benchmarking Python..."
+time python -OO Python/main.py
 echo
